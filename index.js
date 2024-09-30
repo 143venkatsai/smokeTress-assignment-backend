7e29647be58549b6b501e44b4a59eb2467ad0569
143venkatsai <venkatsaipelluru143@gamil.com>
2024-09-30 11:24:16 +0530

    commited the changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const db = require('./database');
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 
 app.use(cors({origin:'http://localhost:3000'})); // frontend URL
 app.use(bodyParser.json());
